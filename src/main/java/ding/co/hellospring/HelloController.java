d78441594f22c0f0db235915eab842d9ab7f1cb5
Bruce Han <intragerhan@gmail.com>
2026-08-16 10:55:57 +0900

    JSON응답 만들기

--- src/main/java/ding/co/hellospring/HelloController.java
+++ src/main/java/ding/co/hellospring/HelloController.java
@@ -3,11 +3,26 @@ package ding.co.hellospring;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello Spring Boot";
+    }
+
+    @GetMapping("/user")
+    public User getUser() {
+        User user = new User("Dingco", 30);
+        return user;
+    }
+
+    @GetMapping("/users")
+    public List<User> getUserList() {
+        User user1 = new User("Dingco", 30);
+        User user2 = new User("Dingco", 40);
+        return List.of(user1, user2);
     }
 }
