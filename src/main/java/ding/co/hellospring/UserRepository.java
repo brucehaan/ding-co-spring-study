80af9788f6ae777c37fe30fdf7f12f5c58750201
Bruce Han <intragerhan@gmail.com>
2026-08-17 21:22:23 +0900

    쿼리 생성기

--- src/main/java/ding/co/hellospring/UserRepository.java
+++ src/main/java/ding/co/hellospring/UserRepository.java
@@ -1,6 +1,15 @@
 package ding.co.hellospring;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByName(String name);
+
+    @Query("select u from User u where u.email = ?1 and u.name = ?2")
+    List<User> findUserCustom(String email, String name);
+
+
 }
