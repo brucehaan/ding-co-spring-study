7937c6b920de79133605662954b83e465d5daf51
Bruce Han <intragerhan@gmail.com>
2026-08-17 21:37:44 +0900

    쿼리 생성기

--- src/main/java/ding/co/hellospring/UserRepository.java
+++ src/main/java/ding/co/hellospring/UserRepository.java
@@ -8,8 +8,28 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByName(String name);
 
-    @Query("select u from User u where u.email = ?1 and u.name = ?2")
-    List<User> findUserCustom(String email, String name);
+    /*
+    1. AND 조건 : 이름이 같고(AND) 나이가 같은 사람
+    SQL : ... WHERE name = ? AND age = ?
+     */
+    List<User> findByNameAndAge(String name, int age);
 
+    /*
+    2. OR 조건 : 이름이 같거나(OR) 나이가 많은 사람
+    SQL : ... WHERE name = ? OR age > ?
+     */
+    List<User> findByNameOrAgeGreaterThan(String name, int age);
+
+    /*
+    3. 정렬(OrderBy) : 나이 기준으로 내림차순(Desc) 정렬
+    SQL : ... ORDER BY age DESC
+     */
+    List<User> findByAgeOrderByAgeDesc(int age);
+
+    /*
+    4. 제한(Limit) : 상위 3명만 가져오기
+    SQL : ... LIMIT 3
+     */
+    List<User> findTop3ByAgeGreaterThan(int age);
 
 }
